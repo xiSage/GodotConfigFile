82b7ecea254ea2577250eca91949cfa4ab27a815
xiSage <57551864+xiSage@users.noreply.github.com>
2025-11-18 21:55:46 +0800

    添加项目文件。

---
+++ GodotConfigFile/ConfigFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GodotConfigFile
+{
+    public class ConfigFile
+    {
+        public Variant GetValue(string section, string key, Variant defaultValue = new Variant())
+        {
+            throw new NotImplementedException();
+		}
+
+        public bool HasSection(string section)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasSectionKey(string section, string key)
+        {
+            throw new NotImplementedException();
+		}
+
+        public string[] GetSections()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetSectionKeys(string section)
+        {
+            throw new NotImplementedException();
+		}
+
+        public void Load(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Parse(string data)
+        {
+            throw new NotImplementedException();
+        }
+	}
+}
---
+++ GodotConfigFile/Variant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GodotConfigFile
+{
+	public struct Variant
+	{
+
+	}
+}
